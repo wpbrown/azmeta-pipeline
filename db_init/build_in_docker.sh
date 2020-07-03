7e0a4943de65846333bf0f24990776b6679f4f3b
Will Brown <will@rebeagle.com>
2020-07-03 16:35:15 -0400

    add release wf

--- db_init/build_in_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-mkdir build
-
-cp build.sh requirements.txt __main__.py build
-docker run -it -v $PWD/build:/app --user 1000 --rm -w /app mcr.microsoft.com/azure-cli:2.0.80 bash -c 'export HOME=/tmp; export PATH=$HOME/.local/bin:$PATH; python3 -m pip install -U --user pip; /app/build.sh --no-zip'
-python3 -m zipapp --compress --output=build/db_init.pyz build/build
--- db_init/build_pyz.sh
+++ db_init/build_pyz.sh
@@ -7,8 +7,5 @@ mkdir -p $WORK_DIR
 cp $DIR/__main__.py $WORK_DIR
 pip install -r $DIR/requirements.txt --target $WORK_DIR
 find $WORK_DIR \( -name '*.dist-info' -o -name '__pycache__' \) -prune -exec rm -r '{}' \;
-touch $WORK_DIR/azure/__init__.py
-
-if [[ $1 != '--no-zip' ]]; then
-    python3 -m zipapp --compress --output=build/db_init.pyz $WORK_DIR
-fi
+touch $WORK_DIR/azure/__init__.py # Workaround some problem with the .pth for Azure Namesapce.
+python3 -m zipapp --compress --output=build/db_init.pyz $WORK_DIR
