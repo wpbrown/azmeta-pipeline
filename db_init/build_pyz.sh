16d2462be66dc059ad071cf94a66ca8f80a23734
Will Brown <will@rebeagle.com>
2020-09-03 20:35:06 +0000

    new export format support

--- db_init/build_pyz.sh
+++ db_init/build_pyz.sh
@@ -5,7 +5,7 @@ WORK_DIR="build/working"
 rm -rf build
 mkdir -p $WORK_DIR
 cp $DIR/__main__.py $WORK_DIR
-pip install -r $DIR/requirements.txt --target $WORK_DIR
+pip3 install -r $DIR/requirements.txt --target $WORK_DIR
 find $WORK_DIR \( -name '*.dist-info' -o -name '__pycache__' \) -prune -exec rm -r '{}' \;
 touch $WORK_DIR/azure/__init__.py # Workaround some problem with the .pth for Azure Namesapce.
 python3 -m zipapp --compress --output=build/db_init.pyz $WORK_DIR
